6163524a663e30399e51bce672e2f5987e052bd6
m7mdhussam <bplan555@gmail.com>
2023-07-08 16:44:16 +0300

    h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AlarmListComponent } from './dashboard/alarm-list/alarm-list.component';
+import { SiteViewComponent } from './dashboard/site-view/site-view.component';
+import { FuelLevelComponent } from './dashboard/fuel-level/fuel-level.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlarmListComponent,
+    SiteViewComponent,
+    FuelLevelComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
